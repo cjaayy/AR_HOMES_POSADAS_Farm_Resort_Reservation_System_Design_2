8d9e8e9280d964019495834c499b3399c9db9364
Christian Jay Mandani <mjhayxinadnam@gmail.com>
2025-10-21 19:14:04 +0800

    fix: Update batch file URL and enhance login debug log with additional entries

--- open_ar_homes.bat
+++ open_ar_homes.bat
@@ -1,3 +1,3 @@
 @echo off
 REM Open the AR Homes Posadas Farm Resort Reservation System in the default browser
-start "" "http://localhost/Design_2_Ayen_Design_AR_Homes_Posadas_Farm_Resort_Reservation_System/index.html"
+start "" "http://localhost/AR_Homes_Posadas_Farm_Resort_Reservation_System_Design_2/index.html"
